cd98eed52187810d309ea11c18c36c8cc54dced5
Hey! Let's Code <39938462+heyletscode@users.noreply.github.com>
2020-11-03 07:55:27 +0530

    server folder added

---
+++ server/call.js
@@ -0,0 +1,80 @@
+let localVideo = document.getElementById("local-video")
+let remoteVideo = document.getElementById("remote-video")
+
+localVideo.style.opacity = 0
+remoteVideo.style.opacity = 0
+
+localVideo.onplaying = () => { localVideo.style.opacity = 1 }
+remoteVideo.onplaying = () => { remoteVideo.style.opacity = 1 }
+
+let peer
+function init(userId) {
+    peer = new Peer(userId, {
+        host: '192.168.43.242',
+        port: 9000,
+        path: '/videocallapp'
+    })
+
+    listen()
+}
+
+let localStream
+function listen() {
+    peer.on('call', (call) => {
+
+        navigator.getUserMedia({
+            audio: true, 
+            video: true
+        }, (stream) => {
+            localVideo.srcObject = stream
+            localStream = stream
+
+            call.answer(stream)
+            call.on('stream', (remoteStream) => {
+                remoteVideo.srcObject = remoteStream
+
+                remoteVideo.className = "primary-video"
+                localVideo.className = "secondary-video"
+
+            })
+
+        })
+        
+    })
+}
+
+function startCall(otherUserId) {
+    navigator.getUserMedia({
+        audio: true,
+        video: true
+    }, (stream) => {
+
+        localVideo.srcObject = stream
+        localStream = stream
+
+        const call = peer.call(otherUserId, stream)
+        call.on('stream', (remoteStream) => {
+            remoteVideo.srcObject = remoteStream
+
+            remoteVideo.className = "primary-video"
+            localVideo.className = "secondary-video"
+        })
+
+    })
+}
+
+function toggleVideo(b) {
+    if (b == "true") {
+        localStream.getVideoTracks()[0].enabled = true
+    } else {
+        localStream.getVideoTracks()[0].enabled = false
+    }
+} 
+
+function toggleAudio(b) {
+    if (b == "true") {
+        localStream.getAudioTracks()[0].enabled = true
+    } else {
+        localStream.getAudioTracks()[0].enabled = false
+    }
+} 
